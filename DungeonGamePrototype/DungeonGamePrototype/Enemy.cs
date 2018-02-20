5f46efd0e3780e66e5a350479e2dd436090f9164
RickyVarun <36651917+RickyVarun@users.noreply.github.com>
2018-02-19 19:44:11 -0800

    i added variables in this code check it

--- DungeonGamePrototype/DungeonGamePrototype/Enemy.cs
+++ DungeonGamePrototype/DungeonGamePrototype/Enemy.cs
@@ -6,5 +6,24 @@ namespace DungeonGamePrototype
 {
     class Enemy
     {
+        //variables for enemies
+        
+        //the damage dealt to the player
+        int iDmageToPlayer;
+        
+        //level of the enemy
+        int iLevel;
+        
+        //health of enemy
+        int iHealth;
+        
+        //tells the if the enemy is alive or dead
+        bool bDeadorAlive;
+        
+        
+    
+    
+    
+    
     }
 }
