5471dfca8147a8f48d56dc7745e16db5ecb542bd
RickyVarun <36651917+RickyVarun@users.noreply.github.com>
2018-02-19 20:01:18 -0800

    here are bunch of changes for hero

--- DungeonGamePrototype/DungeonGamePrototype/Hero.cs
+++ DungeonGamePrototype/DungeonGamePrototype/Hero.cs
@@ -6,5 +6,29 @@ namespace DungeonGamePrototype
 {
     class Hero
     {
+        // Hero's Health
+        int iHealth;
+        
+        //Heroes normal Attacks:
+        //-sword swing
+        //-bow attack
+        //Special attacck
+        //-super smash
+        //- ultra kick
+        
+        
+        //functions
+        
+        //sword attack/bow attack
+        public int SwordAttack(int iEnemyHealth)
+        {
+            
+            return iEnemyHealth;
+        
+        
+        } 
+        
+        // special attacks
+       
     }
 }
